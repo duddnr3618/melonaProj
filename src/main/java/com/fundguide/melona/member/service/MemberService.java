f1fa505af52d8de7278eb208279251ec1e374135
dongchuelKim <terrinens@gmail.com>
2023-09-21 14:35:03 +0900

    feat : 각 게시판 필터처리 구현중
    Signed-off-by: dongchuelKim <terrinens@gmail.com>

--- src/main/java/com/fundguide/melona/member/service/MemberService.java
+++ src/main/java/com/fundguide/melona/member/service/MemberService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class MemberService {
+
     private final MemberRepository memberRepository;
 
     public Page<MemberEntity> getMemberPage(Pageable pageable) {
