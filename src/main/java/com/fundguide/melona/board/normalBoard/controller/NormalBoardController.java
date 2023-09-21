dd31f2233f257049f373478c0532ceda2b581f20
dongchuelKim <terrinens@gmail.com>
2023-09-21 14:35:03 +0900

    feat : 인계를 위한 중간 커밋
    Signed-off-by: dongchuelKim <terrinens@gmail.com>

--- src/main/java/com/fundguide/melona/board/normalBoard/controller/NormalBoardController.java
+++ src/main/java/com/fundguide/melona/board/normalBoard/controller/NormalBoardController.java
@@ -42,7 +42,7 @@ public class NormalBoardController {
         return "board/viewsearchboard";
     }
 
-    /**노말보드 상세 페이지 메서드*/
+    /**노말보드 상세 페이지 이동*/
     @GetMapping("/viewDetail/{boardId}")
     public String boardViewDetail(Model model, @PathVariable(name = "boardId") Long boardId) {
         NormalBoardEntity boardEntity = queryService.onlyViewDetailNormalBoard(boardId);
