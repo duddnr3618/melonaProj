28ac97ce01111714b5ce5b9776988d0de5e4c78d
dongchuelKim <terrinens@gmail.com>
2023-09-21 10:35:32 +0900

    del : 쓸모없는 어노테이션 삭제
    Signed-off-by: dongchuelKim <terrinens@gmail.com>

--- src/main/java/com/fundguide/melona/board/common/dto/BoardSearchDTO.java
+++ src/main/java/com/fundguide/melona/board/common/dto/BoardSearchDTO.java
@@ -1,11 +1,4 @@
 package com.fundguide.melona.board.common.dto;
 
-import lombok.Builder;
-
-@Builder
-public record BoardSearchDTO(
-        String searchKeyword
-        , String searchOption
-) {
-
-}
+/**page에 대한 null값은 반드시 분기처리할것*/
+public record BoardSearchDTO(String searchKeyword, String searchOption, int searchPageValue) { }
