0e5e7c36ff046cd03c887bc023b05708e2c5a142
BeomjunKim123 <gkgk9753@gmail.com>
2023-09-25 15:52:50 +0900

    웹 소켓 관련 설정

---
+++ src/main/java/com/fundguide/melona/WebSocket/config/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package com.fundguide.melona.WebSocket.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+        /*
+         * withSockJS()
+         * 는 웹소켓을 지원하지 않는 브라우저에
+         * 폴백 옵션을 활성화하는데 사용됩니다.
+         * */
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // "/ app"로 시작하는 메시지가 메시지 처리 방법으로 라우팅되어야 함을 정의합니다
+        registry.setApplicationDestinationPrefixes("/app");
+        //"/ topic"으로 시작하는 메시지가 메시지 브로커로 라우팅되도록 정의합니다.
+        // 메시지 브로커는 특정 주제를 구독 한 연결된 모든 클라이언트에게 메시지를 브로드 캐스트합니다.
+        registry.enableSimpleBroker("/topic");
+    }
+
+
+}
---
+++ src/main/java/com/fundguide/melona/WebSocket/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.fundguide.melona.WebSocket.controller;
+
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+//    public GreetingResponse greeting(final HelloMessageRequest message) throws InterruptedException {
+//        // simulated delay
+//        // 클라이언트가 메시지를 보낸 후 서버가 메시지를 비동기식으로 처리하는 데 필요한 시간만큼 오래 걸릴 수 있음을 보여주기 위한 것
+//        Thread.sleep(1000);
+//
+//        return new GreetingResponse("Hello, " + HtmlUtils.htmlEscape(message.name()) + " !!");
+//    }
+
+
+}
