e10dfc4814bf576be28f24bcd328be18e8bd932d
dongchuelKim <terrinens@gmail.com>
2023-09-25 12:47:18 +0900

    docs : 메서드 설명을 위한 문서 추가
    Signed-off-by: dongchuelKim <terrinens@gmail.com>

--- src/main/java/com/fundguide/melona/management/service/filter/FilterCategoryHandler.java
+++ src/main/java/com/fundguide/melona/management/service/filter/FilterCategoryHandler.java
@@ -3,6 +3,7 @@ package com.fundguide.melona.management.service.filter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/**각 보드에 대한 매니지먼트를 위한 인터페이스*/
 public interface FilterCategoryHandler {
 
     default Page<?> handleFilterCategory(String filter, Pageable pageable) {
@@ -23,7 +24,10 @@ public interface FilterCategoryHandler {
         }
     }
 
+    /**경고 필터 처리한 페이지를 전달할것*/
     Page<?> waringPage();
+    /**블락 필터 처리한 페이지를 전달할것*/
     Page<?> blockPage();
+    /**모든 페이지를 전달할것*/
     Page<?> allPage();
 }
