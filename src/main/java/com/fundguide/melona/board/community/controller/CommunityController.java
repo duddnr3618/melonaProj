41857848124ebf4901d304f253769e6ad445e30c
Choi <duddnr3618@gmail.com>
2023-10-09 05:04:19 +0900

    23.10.09 커뮤니티 페이징

--- src/main/java/com/fundguide/melona/board/community/controller/CommunityController.java
+++ src/main/java/com/fundguide/melona/board/community/controller/CommunityController.java
@@ -1,10 +1,13 @@
 package com.fundguide.melona.board.community.controller;
 
 import com.fundguide.melona.board.community.dto.CommunityDto;
+import com.fundguide.melona.board.community.entity.CommunityEntity;
 import com.fundguide.melona.board.community.service.CommunityService;
 import com.fundguide.melona.member.service.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,9 +28,25 @@ public class CommunityController {
 
     /* 게시글 리스트 페이지 */
     @GetMapping("/list")
-    public String list(Model model, @PageableDefault(page = 1) Pageable pageable) {
+    public String list(Model model,
+                       @PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC)Pageable pageable,
+                       @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        Page <CommunityEntity> list = communityService.boardList(pageable);
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4,1);
+        int endPage = Math.min(nowPage + 5,list.getTotalPages()) ;
+        if (customUserDetails == null) {
+            return "redirect:/member/loginForm";
+        } else {
+            String memberName = customUserDetails.getMemberEntity().getMemberName();
+            model.addAttribute("list", list);
+            model.addAttribute("userInfo", memberName);
+            model.addAttribute("nowPage",nowPage);
+            model.addAttribute("startPage",startPage);
+            model.addAttribute("endPage",endPage);
 
-        return "board/community/list";
+            return "board/community/list";
+        }
     }
 
     /* 게시글 작성폼 */
