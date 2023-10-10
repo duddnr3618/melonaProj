4416509bda3267b804e7d5fd08f295938dc64be1
BYH <231447@naver.com>
2023-10-10 18:13:36 +0900

    10-10 노말보드 커뮤니티 보드 최신순 작성 커밋

--- src/main/java/com/fundguide/melona/board/community/service/CommunityService.java
+++ src/main/java/com/fundguide/melona/board/community/service/CommunityService.java
@@ -5,6 +5,8 @@ import com.fundguide.melona.board.community.entity.CommunityEntity;
 import com.fundguide.melona.board.community.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,5 +21,8 @@ public class CommunityService {
     public void writePro(CommunityDto communityDto) {
         CommunityEntity communityEntity = CommunityEntity.of(communityDto);
         communityRepository.save(communityEntity);
+    }
+    public Page<CommunityEntity> getLatestCommunityBoard(Pageable pageable) {
+        return communityRepository.findAll(pageable); // 최신순으로 정렬된 CommunityBoard 게시글을 반환합니다.
     }
 }
--- src/main/java/com/fundguide/melona/member/controller/MyPageController.java
+++ src/main/java/com/fundguide/melona/member/controller/MyPageController.java
@@ -1,53 +1,64 @@
 package com.fundguide.melona.member.controller;
 
-import com.fundguide.melona.board.community.dto.CommunityDto;
 import com.fundguide.melona.board.community.entity.CommunityEntity;
-import com.fundguide.melona.board.community.repository.CommunityRepository;
 import com.fundguide.melona.board.community.service.CommunityService;
-import com.fundguide.melona.board.leaderboard.dto.LeaderBoardDto;
-import com.fundguide.melona.board.leaderboard.service.LeaderBoardService;
-import com.fundguide.melona.board.like.repository.LikeRepository;
-import com.fundguide.melona.member.entity.MemberEntity;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fundguide.melona.board.normalBoard.entity.NormalBoardEntity;
+import com.fundguide.melona.board.normalBoard.service.NormalBoardQueryService;
+import com.fundguide.melona.member.service.CustomUserDetails;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 
-    @Controller
+import org.springframework.data.domain.Pageable;
+
+@Controller
     @RequestMapping("/mypage")
     public class MyPageController {
-        private final CommunityRepository communityRepository;
-        private final LikeRepository likeRepository;
-
-        @Autowired
-        public MyPageController(CommunityRepository communityRepository, LikeRepository likeRepository) {
-            this.communityRepository = communityRepository;
-            this.likeRepository = likeRepository;
+        private final NormalBoardQueryService normalBoardQueryService;
+        private final CommunityService communityService;
+        public MyPageController(NormalBoardQueryService normalBoardQueryService ,CommunityService communityService) {
+            this.normalBoardQueryService = normalBoardQueryService;
+            this.communityService = communityService;
         }
 
-        @GetMapping("/myPosts")
-        public String myPosts(@RequestParam(defaultValue = "createdTime") String orderBy,
-                              Model model, Pageable pageable) {
-            Page<CommunityEntity> communityPage;
+        // ...
 
-            if ("mostLiked".equals(orderBy)) {
-                List<Long> mostLikedCommunityIds = likeRepository.findMostLikedCommunityIds();
-                communityPage = communityRepository.findAllByIdInOrderByCreatedTimeDesc(mostLikedCommunityIds, pageable);
-            } else {
-                communityPage = communityRepository.findAllByIdInOrderByCreatedTimeDesc(pageable);
-            }
+        @GetMapping("/normalboard")
+        public String myPageNormalBoard(Model model) {
+            // 최신순 정렬을 위한 Pageable 생성
+            Pageable pageable = PageRequest.of(0, 15, Sort.by(Sort.Order.desc("createdTime")));
 
-            model.addAttribute("communityPage", communityPage);
-            return "mypage/myPosts";
+            // 내가 작성한 NormalBoard 게시글 조회
+            Page<NormalBoardEntity> pagingNormalBoard = normalBoardQueryService.onlyViewPageNormalBoard(pageable);
+
+            model.addAttribute("pagingBoard", pagingNormalBoard);
+
+            return "mypage/normal_board"; //
         }
+    /* 내가 작성한 CommunityBoard 게시글 목록 페이지 */
+    @GetMapping("/myCommunityBoard")
+    public String myCommunityBoard(Model model, @PageableDefault(page = 1) Pageable pageable,
+                                   @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        if (customUserDetails == null) {
+            return "member/loginForm"; // 로그인되어 있지 않다면 로그인 페이지로 리다이렉트
+        }
+
+        String userName = customUserDetails.getMemberEntity().getMemberName();
+        model.addAttribute("userName", userName);
+
+        Page<CommunityEntity> pagingCommunityBoard = communityService.getLatestCommunityBoard(pageable);
+        model.addAttribute("pagingCommunityBoard", pagingCommunityBoard);
+
+        return "mypage/myCommunityBoard";
     }
+
+}
+
 
