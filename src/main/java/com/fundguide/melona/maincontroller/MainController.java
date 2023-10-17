8d774f50eac1b0c12362063dcd9cd762c936ba81
Choi <duddnr3618@gmail.com>
2023-10-17 20:41:23 +0900

    10.17 last 커밋

--- src/main/java/com/fundguide/melona/maincontroller/MainController.java
+++ src/main/java/com/fundguide/melona/maincontroller/MainController.java
@@ -1,17 +1,20 @@
 package com.fundguide.melona.maincontroller;
 
+import com.fundguide.melona.member.service.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
   @GetMapping("/")
-
   public String index () {
 
     return "index";
   }
+
 
 
 
